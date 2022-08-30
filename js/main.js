a44fe8cdc2d1d959090783ea60466a89c3cafe24
alex varshavsky <ledav88881@gmail.com>
2022-08-30 00:33:14 -0400

    fixed form styling and added form validation

--- js/main.js
+++ js/main.js
@@ -15,19 +15,52 @@ tabs.forEach(tab => {
             }
 
         })
-
         target.classList.add('active')
     })
 })
 
-
 function CopyToClipboard(text, id) {
-
     /* Copy the text inside the text field */
     navigator.clipboard.writeText(text);
     const alert = document.getElementById(id).style.display = "inline";
 
     setTimeout(function () {
         document.getElementById(id).style.display = "none";
+    }, 2000);
+}
+
+function validateForm() {
+    console.log("form validate");
+    let name = document.forms["contactForm"]["name"];
+    let email = document.forms["contactForm"]["email"];
+    let message = document.forms["contactForm"]["message"];
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (name.value == null || email.value == null || message.value == null) {
+        alert("Please fill out all fields");
+        return false;
+    } else if (email.value.match(validRegex)) {
+        name.value = "";
+        email.value = "";
+        message.value = "";
+        showPopup();
+    } else {
+        const alert = document.getElementById("alert3").style.display = "inline";
+        setTimeout(function () {
+            document.getElementById("alert3").style.display = "none";
+        }, 3000);
+    }
+}
+
+function showPopup() {
+    var popup = document.getElementById("email-popup-container");
+    var button = document.getElementById("button-container-center");
+    popup.style.display = "flex";
+    button.style.display = "none";
+
+    setTimeout(function () {
+        popup.style.display = "none";
+        button.style.display = "flex";
     }, 2000);
 }
